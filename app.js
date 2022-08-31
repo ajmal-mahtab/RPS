bc272eab51853cbbb3abd4638f91d3f04e1ac0b4
ajmal-mahtab <109355224+ajmal-mahtab@users.noreply.github.com>
2022-08-31 15:10:45 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+let player;
+let computer;
+let result;
+let buttons = document.querySelectorAll(".btn");
+
+let computerChoice = () => {
+  let random = Math.ceil(Math.random() * 3);
+  switch (random) {
+    case 1:
+      return "ROCK";
+      break;
+    case 2:
+      return "PAPER";
+      break;
+    case 3:
+      return "SCISSORS";
+  }
+};
+
+let finalResult = () => {
+  if (computer == "ROCK" && player == "PAPER") {
+    return "YOU WIN!";
+  } else if (computer == "ROCK" && player == "SCISSORS") {
+    return "YOU LOOSE!";
+  } else if (computer == "PAPER" && player == "SCISSORS") {
+    return "YOU WIN!";
+  } else if (computer == "PAPER" && player == "ROCK") {
+    return "YOU LOOSE!";
+  }
+  if (computer == "SCISSORS" && player == "ROCK") {
+    return "YOU WIN!";
+  } else if (computer == "SCISSORS" && player == "PAPER") {
+    return "YOU LOOSE!";
+  } else if (computer == player) {
+    return "DRAW!";
+  }
+};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    player = button.textContent;
+    computer = computerChoice();
+    result = finalResult();
+    document.querySelector(".computer").textContent = `Computer:  ${computer}`;
+    document.querySelector(".player").textContent = `You:  ${player}`;
+    document.querySelector(".result").textContent = `Result:  ${result}`;
+  });
+});
